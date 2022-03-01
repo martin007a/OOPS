ff9f9c40615bc74127a641d5dbb902f3554db15d
slavicek.martin <slaviekmartin@post.cz>
2022-03-01 11:36:02 +0100

    Add project files.

---
+++ OOPS/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOPS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Vehicle auto1 = new Vehicle("Škoda");                 //hodnota nul
+            auto1.Type = "Osobní Auto";
+
+            Vehicle auto2 = new Vehicle("VW");
+            auto2.Type = "Dodávka";
+        }
+    }   
+
+}
